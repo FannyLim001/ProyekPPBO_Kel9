5f729dffeb07a67fe43fd951a2f9d6c4c3309156
FannyLim001 <85084905+FannyLim001@users.noreply.github.com>
2021-07-08 20:32:43 +0700

    Add files via upload

---
+++ tokoOptik_kel9_1tid/src/tokooptik_kel9_1tid/TokoOptik_kel9_1tid.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tokooptik_kel9_1tid;
+
+/**
+ *
+ * @author FANNY
+ */
+public class TokoOptik_kel9_1tid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

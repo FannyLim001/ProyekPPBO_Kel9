ee0d68f1e0bde906db39f7a22af54d2d806f0a7f
Fanny <85084905+FannyLim001@users.noreply.github.com>
2021-07-16 08:17:06 +0700

    new db

--- toko_optik.sql
+++ toko_optik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jul 2021 pada 16.17
+-- Waktu pembuatan: 16 Jul 2021 pada 03.15
 -- Versi server: 10.4.16-MariaDB
 -- Versi PHP: 7.4.12
 
@@ -24,75 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_transaksi`
---
-
-CREATE TABLE `detail_transaksi` (
-  `id_transaksi` int(11) NOT NULL,
-  `id_produk` int(11) NOT NULL,
-  `jml_produk` int(11) NOT NULL,
-  `total_biaya` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `member`
---
-
-CREATE TABLE `member` (
-  `id_member` int(11) NOT NULL,
-  `nama` varchar(45) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `no_hp` char(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `produk`
---
-
-CREATE TABLE `produk` (
-  `id_produk` int(11) NOT NULL,
-  `nama_produk` varchar(45) NOT NULL,
-  `kategori_produk` varchar(30) NOT NULL,
-  `brand` varchar(30) NOT NULL,
-  `id_supplier` int(11) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `produk`
---
-
-INSERT INTO `produk` (`id_produk`, `nama_produk`, `kategori_produk`, `brand`, `id_supplier`, `harga`) VALUES
-(1, 'F ZU YD9601 C2 51', 'Frame', 'Zulu', 1, 340000),
-(2, 'NIKE F NJ 5009AF 223 48', 'Frame', 'Nike', 1, 863000);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `supplier`
---
-
-CREATE TABLE `supplier` (
-  `id_supplier` int(11) NOT NULL,
-  `nama` varchar(45) NOT NULL,
-  `alamat` varchar(30) NOT NULL,
-  `no_hp` char(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `supplier`
---
-
-INSERT INTO `supplier` (`id_supplier`, `nama`, `alamat`, `no_hp`) VALUES
-(1, 'ZARA', 'Jl. Tanah Abang', '0812345678');
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `transaksi`
 --
 
@@ -100,86 +31,43 @@ CREATE TABLE `transaksi` (
   `id_transaksi` int(11) NOT NULL,
   `id_member` int(11) NOT NULL,
   `id_produk` int(11) NOT NULL,
-  `tgl_transaksi` date NOT NULL
+  `jml_beli` int(11) NOT NULL,
+  `tgl_beli` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_member`, `id_produk`, `jml_beli`, `tgl_beli`) VALUES
+(1, 1, 1, 2, '2021-07-01'),
+(2, 2, 2, 2, '2021-07-06');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `detail_transaksi`
---
-ALTER TABLE `detail_transaksi`
-  ADD KEY `fk_id_transaksi` (`id_transaksi`),
-  ADD KEY `fk_id_produk2` (`id_produk`);
-
---
--- Indeks untuk tabel `member`
---
-ALTER TABLE `member`
-  ADD PRIMARY KEY (`id_member`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indeks untuk tabel `produk`
---
-ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id_produk`),
-  ADD KEY `fk_id_supplier` (`id_supplier`);
-
---
--- Indeks untuk tabel `supplier`
---
-ALTER TABLE `supplier`
-  ADD PRIMARY KEY (`id_supplier`);
-
---
 -- Indeks untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `fk_id_member` (`id_member`),
-  ADD KEY `fk_id_produk` (`id_produk`);
+  ADD KEY `fk_id_produk` (`id_produk`),
+  ADD KEY `fk_id_member` (`id_member`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
---
-ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `supplier`
---
-ALTER TABLE `supplier`
-  MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
-
---
--- Ketidakleluasaan untuk tabel `detail_transaksi`
---
-ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `fk_id_produk2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_id_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON UPDATE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `produk`
---
-ALTER TABLE `produk`
-  ADD CONSTRAINT `fk_id_supplier` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id_supplier`) ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `transaksi`
